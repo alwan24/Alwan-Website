2f6d34b3ac87325f3d10b1445449339e7fdd1645
alwan24 <116695300+alwan24@users.noreply.github.com>
2022-11-22 15:11:54 +0530

    Update login.js

--- login/login.js
+++ login/login.js
@@ -4,7 +4,7 @@ function login(){
 
     if(username == "Basith" && password == 1023 || username == "Salim" && password == 1038 || username == "Swabeeh" && password == 1035 ||username == "Shamil" && password == 1029
     || username == "Naseef" && password == 1022  || username == "Shajeeh" && password == 1033 ||  username == "Aslah" && password == 1025 ||  username == "Shifin" && password == 1037
-    ||  username == "Rubaib" && password == 1012 ||  username == "Shinas" && password == 1028 ||  username == "Fadil" && password == 1015 ||  username == "Vasil" && password == 1004
+    ||  username == "Rubaib" && password == 1012 ||  username == "Shinas" && password == 1028 ||  username == "Fadil" && password == 1015 ||  username == "Vasil_cs" && password == 4321
     ||  username == "Salsal" && password == 1002 || username == "Sinan" && password == 1005 ||  username == "Sabeeh" && password == 1036 ||  username == "Ashhad" && password == 1032
     || username == "Nafih" && password == 1016 || username == "Rabeeh" && password == 1026  || username == "Shabeer" && password == 1010 || username == "Ajumal" && password == 1018 
     || username == "Murshid" && password == 1014 || username == "Aflah" && password == 1006 || username == "Shammas" && password == 1041 || username == "Sahal" && password == 1031
